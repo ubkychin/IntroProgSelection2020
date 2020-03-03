08b74f1845c2610844d0a92964f8b6ec017e1d22
Anh Nguyen <anguyen@swin.edu.au>
2020-03-03 14:07:37 +1100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IntroProgSelection2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            age = 18;
+            int legalAge = 18;
+
+            if(age >= legalAge) 
+            {
+                Console.WriteLine("DRINK");
+                Console.WriteLine("DRIVE");
+            } 
+            else 
+            {
+                Console.WriteLine("DON'T DRINK");
+                Console.WriteLine("DON'T DRIVE WITHOUT THE L-PLATE");
+            }
+
+            Console.WriteLine("PROGRAM FINISHED!");
+        }
+    }
+}
